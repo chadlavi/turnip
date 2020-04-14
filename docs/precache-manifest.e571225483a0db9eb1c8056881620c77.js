ac81d2d2d0c07f322a21ee6f331b1c0a9313886b
Chad Lavimoniere <chadlavimoniere@Chads-2020-Air.home>
2020-04-14 00:20:33 -0400

    edit build to target a docs directory

---
+++ docs/precache-manifest.e571225483a0db9eb1c8056881620c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249cb37d7ff4421f4a58fe63a7e7797a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df148d97b5020803d912",
+    "url": "/static/css/main.038043c3.chunk.css"
+  },
+  {
+    "revision": "57fd33dac56e6ec741a3",
+    "url": "/static/js/2.968f1ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.968f1ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df148d97b5020803d912",
+    "url": "/static/js/main.f9846ab3.chunk.js"
+  },
+  {
+    "revision": "da0208461aad48f900be",
+    "url": "/static/js/runtime-main.1286bedc.js"
+  }
+]);
